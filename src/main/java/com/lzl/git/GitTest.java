d35b13bb1625dd5ba2ef102e4afc04ae0312fa88
xiaozhi <1446625308@qq.com>
2022-03-06 15:53:58 +0800

    first commit

---
+++ src/main/java/com/lzl/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.lzl.git;
+
+/**
+ * @author liuzhilong
+ * @create 2022-03-06 15:50
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
